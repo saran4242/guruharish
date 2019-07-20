e109d2483b431f87e1ec31ad0b5b80e98efa664c
saran4242 <gurusharanlatha@gmail.com>
2019-07-20 18:00:52 +0530

    guruharish

---
+++ src/guruharish/Testing.java
@@ -0,0 +1,21 @@
+package guruharish;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Testing {
+	
+	
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\SARAVANAKUMAR\\eclipse-workspace\\Saravanan\\guruharish\\Driver\\chromedriver.exe");
+
+		WebDriver driver = new ChromeDriver();	
+	
+	    driver.get("https://www.facebook.com");
+	
+	}
+	
+	
+	
+
+}
